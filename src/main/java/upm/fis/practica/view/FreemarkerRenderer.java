62861a7c46a069c0cb3a919cb49c632f8a9851c3
Andrea Cimmino Arriaga <cimmino@fi.upm.es>
2022-05-06 10:27:27 +0200

    views updated

--- src/main/java/upm/fis/practica/view/FreemarkerRenderer.java
+++ src/main/java/upm/fis/practica/view/FreemarkerRenderer.java
@@ -2,7 +2,14 @@ package upm.fis.practica.view;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import spark.ModelAndView;
 import spark.template.freemarker.FreeMarkerEngine;
 import upm.fis.practica.AppUtils;
@@ -13,10 +20,12 @@ public class FreemarkerRenderer implements ViewRenderer{
 	private FreeMarkerEngine engine;
 	private File viewsDirectory;
 	private static final String VIEWS_PATH = "./src/test/resources/spark/template/freemarker";
-
+	private ObjectMapper mapper;
+	
 	public FreemarkerRenderer() {
 		engine = new FreeMarkerEngine();
 		viewsDirectory = new File(VIEWS_PATH);
+		mapper = new ObjectMapper();
 	}
 	
 	public String render(Map<String,Object> model, String template) throws RenderTemplateException {
@@ -34,6 +43,26 @@ public class FreemarkerRenderer implements ViewRenderer{
 	
 	private boolean fileExists(String name) {
 		return Arrays.asList(viewsDirectory.listFiles()).parallelStream().anyMatch(file -> file.getName().equals(name));
+	}
+	
+	@Override
+	public Map<String, Object> toModel(Object object) {
+		@SuppressWarnings("unchecked")
+		Map<String, Object> model = mapper.convertValue(object, Map.class);
+		return model;
+	}
+	
+	@Override
+	public String render(Object object, String template) throws RenderTemplateException {
+		return render(toModel(object), template);
+	}
+	
+	@Override
+	public Map<String, Object> toModel(List<?> object, String variable) {
+		Map<String,Object> model = new HashMap<>();
+		List<Object> values = object.parallelStream().map(elem -> toModel(elem)).collect(Collectors.toList());
+		model.put(variable, values);
+		return model;
 	}
 	
 }
